7866f0ce458c087c4d49777111e641a200cae0ad
MoeIsLearning <moeislearning@gmail.com>
2022-11-05 01:13:38 +0200

    head tags added to adjust safari ios browser to not zoom on input because i love little jimjim

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,6 +74,12 @@ export default function Index() {
   return (
     //h-screen w-screen
     <div className="chat-box flex flex-col justify-between">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
       {/* {lastMsg && <h1>{JSON.stringify(lastMsg)}</h1>} */}
       <ul
         id="chat-messages"
